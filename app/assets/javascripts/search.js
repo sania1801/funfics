56020f06977ac2bff9f5e56cdc262ab30ccfcfd9
sania1801 <sania18012000@gmail.com>
2019-06-15 07:28:05 +0300

    Add user avatar

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,11 +1,11 @@
 function searchPosts(){
-    console.log('lox');
-    let value = document.getElementById('.main_search_field').value;
-    document.location.href='/posts?val=' + value;
+    console.log(document);
+        var value = document.getElementById('main_search_field').value;
+        document.location.href = '/posts?val=' + value;
 }
 $(document).ready()
 {
-    $(".post_search").click(function(){
+    $("#post_search").click(function(){
         console.log('durak')
         searchPosts();
     })
