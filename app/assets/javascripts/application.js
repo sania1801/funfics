ea1e9847415cbb276960e98be118dbf0cbbdb00f
sania1801 <sania18012000@gmail.com>
2019-09-03 01:27:22 +0300

    First commit, after debug all functions, how a simple user, i can to create/edit/delete posts/chapters, find all information from posts(full-text search), also i can use tagcloud, then, i can to create comments, and more than things. How admin i can create/edit/delete all information of this site, also do this with users, remove/create/edit admin user, and more than. How unauthorized user i can sign up with email confirmation, then i can see posts/comments/chapters, use full-text search, and tagcloud

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+/// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -7,8 +7,9 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// Read Sprockets README (https://github.com/rails/sprocketssprockets-directives) for details
 // about supported directives.
+///
 //= require rails-ujs
 //= require activestorage
 //= require jquery3
@@ -20,5 +21,41 @@
 //= require cable
 //= require rails-ujs
 
+//= require react
+//= require react_ujs
+//= require components
 
+function newPost(){
+document.location.href='/posts/new';
+}
+$(document).ready(function() {
+  $( "#btn_new_post" ).click(function() {
+  newPost();
+});
+});
+
+function oldUser(){
+    document.location.href='/users/sign_in';
+}
+$(document).ready(function() {
+    $( "#btn_sign_in" ).click(function() {
+        oldUser();
+    });
+});
+function newUser(){
+    document.location.href='/users/sign_up';
+}
+$(document).ready(function() {
+    $( "#btn_sign_up" ).click(function() {
+        newUser();
+    });
+});
+function allFunfics(){
+    document.location.href='/posts';
+}
+$(document).ready(function() {
+    $( "#funfics" ).click(function() {
+        allFunfics();
+    });
+});
 
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,14 +1,10 @@
 function searchPosts(){
     var value = $('#post_search').val();
-    console.log(value)
      document.location.href='/posts?val='+value;
-    console.log(document.location.href);
 
 }
 $(document).ready(function() {
-//     console.log('cho?');
     $( "#btn_post_search" ).click(function() {
-        console.log('ne tukai');
         searchPosts();
     });
 });
